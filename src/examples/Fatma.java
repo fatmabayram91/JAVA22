015ff67328dff096c1219cb846f1ff83f2cbc3a0
fatmabayram91 <fatmabayram19901991@gmail.com>
2022-09-20 14:29:04 +0100

    new message

--- src/examples/Fatma.java
+++ src/examples/Fatma.java
@@ -2,6 +2,6 @@ package examples;
 
 public class Fatma {
     public static void main(String[] args) {
-        System.out.println("Hello world from Fatma");
+        System.out.println("Hello world ");
     }
 }
