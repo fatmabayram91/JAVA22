7731ab5dfe9d361dd14bc6b0fd3e2a1827f5a503
fatmabayram91 <fatmabayram19901991@gmail.com>
2022-11-04 10:08:44 +0000

    Ternary

--- src/day06nestedifternaryswitch/NestedTearnary02.java
+++ src/day06nestedifternaryswitch/NestedTearnary02.java
@@ -1,2 +1,32 @@
-package day06nestedifternaryswitch;public class NestedTearnary02 {
+package day06nestedifternaryswitch;
+
+public class NestedTearnary02 {
+
+    public static void main(String[] args) {
+
+     /*
+     Type code to check the password
+     If it has more than 8 characters, initial should be 'i'
+     If it has no more than 8 characters initial should be 'K'  and the last character should be '!'
+     solve the task by using nested-ternary
+      */
+// length () method returns the number of characters in a string
+        //for example: "Ali Can" . length() ==> 7
+
+String pwd = "ia1b3c7xy";
+
+//() ? () :() ;
+
+ String isvalid= (pwd.length()>8) ?
+                (pwd.charAt(0)=='i' ? "Valid password" : "Invalid password") :
+                (pwd.charAt(0)=='K' ? "Valid Password"  : "Invalid password" );
+
+        System.out.println(isvalid);
+
+
+
+
+
+
+    }
 }
--- src/day06nestedifternaryswitch/ReviewTernary01.java
+++ src/day06nestedifternaryswitch/ReviewTernary01.java
@@ -1,2 +1,31 @@
-package day06nestedifternaryswitch;public class ReviewTernary01 {
+package day06nestedifternaryswitch;
+
+public class ReviewTernary01 {
+    public static void main(String[] args) {
+
+    // if an integer is positive print " the integer is positive" otherwise print "the integer is not positive"
+
+//1.Way: solve by using if-else statement
+
+        int num = 10;
+                if(num>0){
+                    System.out.println("the integer is positive");}
+                else{
+                    System.out.println("The integer is not positive");}
+
+//2.Way: ternary
+        //condition   ?   ternary 1   :  ternary 2
+
+        String result = num>0   ? "The integer is positive" : "The integer is not positive";
+
+                System.out.println(result);
+//Write a program to print the minimum of 2 integers on the console . use ternary..
+
+        int a = 100;
+        int b = 100;
+       int min = a<b ? a:b;
+        System.out.println(min);
+
+
+    }
 }
