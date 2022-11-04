df0bb2b4c5ebcc74292414b8546f746a92f32d55
fatmabayram91 <fatmabayram19901991@gmail.com>
2022-11-04 10:22:05 +0000

    Ternary03

---
+++ src/day06nestedifternaryswitch/Ternary03.java
@@ -0,0 +1,30 @@
+package day06nestedifternaryswitch;
+
+import java.sql.SQLOutput;
+
+public class Ternary03 {
+
+    public static void main(String[] args) {
+
+     // You have 2 integers;
+     //if both of the integers are positive do multiplication
+     // if one is negative the othe one is positive return " i do not how to multiply"
+
+        int a = 12;
+        int b = 5;
+        // If your ternary returns different data types for different scenarios,
+        // do not create reserved area for the result, put it directly inside System.out.println()
+
+        System.out.println( a>0 && b>0 ? a*b : " I do not how to multiply");
+
+
+
+
+
+    }
+
+
+
+
+
+}
