c516bfa1496e9aad3a98ee860312145f4d461588
fatmabayram91 <fatmabayram19901991@gmail.com>
2022-11-08 18:54:04 +0000

    String

--- src/day07stringmanipulations/Sm01.java
+++ src/day07stringmanipulations/Sm01.java
@@ -7,12 +7,12 @@ public class Sm01 {
 
     public static void main(String[] args) {
 
-      // print the firs and last character of the given String on the console. "Java is easy" ==>Jy
+        // print the firs and last character of the given String on the console. "Java is easy" ==>Jy
 
-      Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a String");
-        String str = scan.next();
+        String str = scan.nextLine();
 
         // to get characters from a string one by one we have 2 ways;
         // 1.way : using charAt();
@@ -21,11 +21,19 @@ public class Sm01 {
 
         System.out.println(firstChar);
 
-  str.charAt(str.length()-1);
-  char lastChar = str.charAt(str.length()-1);
+
+        char lastChar = str.charAt(str.length() - 1);
         System.out.println(lastChar);
 //Note:if you use '0' inside the charAt() method you will get last character in a string
 
+        //2.Way: using substring()
+        //Note: to get first character by using substring method use str.substring(0,1);
+
+       String firstCharacter = str.substring(0,1);
+       System.out.println(firstCharacter);
+
+       String lastCharacter =  str.substring(str.length()-1, str.length());
+       System.out.println(lastCharacter);
 
 
     }
