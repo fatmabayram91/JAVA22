a3c4881a45927e58dcb427c19cf3b635d11d2af7
fatmabayram91 <fatmabayram19901991@gmail.com>
2022-11-14 19:38:52 +0000

    new message

---
+++ src/day07stringmanipulations/Sn02.java
@@ -0,0 +1,20 @@
+package day07stringmanipulations;
+
+public class Sn02 {
+
+    public static void main(String[] args) {
+
+
+  String str = "Ali Can";
+  String nesStr = str.concat("!");
+        System.out.println("dfs");
+
+
+
+
+
+
+
+
+    }
+}
